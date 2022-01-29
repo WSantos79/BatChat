43903eaf1525b82ea0a4a518ed81f7a6ede20d1c
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2022-01-29 11:35:22 -0800

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,38 +1,38 @@
 export default function MyApp ({ Component, pageProps}) {
-    // roda em todas as páginas !
+  // roda em todas as páginas !
 
-    return (
-    <>
-         <GlobalStyle/>
-        <Component {...pageProps} />
-    </>
+  return (
+  <>
+       <GlobalStyle/>
+      <Component {...pageProps} />
+  </>
 )}
 
 function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
+  return (
+      <style global jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+      }
+      body {
+        font-family: 'Open Sans', sans-serif;
+      }
+      /* App fit Height */ 
+      html, body, #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+      #__next {
+        flex: 1;
+      }
+      #__next > * {
+        flex: 1;
+      }
+      /* ./App fit Height */ 
+    `}</style>
+  );
 }
